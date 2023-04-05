d50990cc16ebde06647d5a45bcef89300f9b03fe
Jonas Meisner <meisnerucph@gmail.com>
2023-04-05 12:31:27 +0200

    ay ay ay

--- halko/halkoMain.py
+++ halko/halkoMain.py
@@ -70,7 +70,7 @@ def main():
 	# Save matrices
 	np.savetxt(args.out + ".eigenvecs", V.T, fmt="%.7f")
 	print(f"Saved eigenvector(s) as {args.out}.eigenvecs")
-	np.savetxt(args.out + ".eigenvals", S**2/float(m), fmt="%.7f")
+	np.savetxt(args.out + ".eigenvals", S**2/float(M), fmt="%.7f")
 	print(f"Saved eigenvalue(s) as {args.out}.eigenvals")
 
 	# Save loadings
