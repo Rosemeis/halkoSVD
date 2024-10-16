ecc04e39844fcf57f11776a4e893ccbd1e551462
Jonas Meisner <meisnerucph@gmail.com>
2024-10-16 10:24:51 +0200

    test

--- setup.py
+++ setup.py
@@ -14,10 +14,6 @@ extensions = [
 ]
 
 setup(
-    packages=["halko"],
-	entry_points={
-		"console_scripts": ["halkoSVD=halko.main:main"]
-	},
     ext_modules=cythonize(extensions),
     include_dirs=[numpy.get_include()]
 )
