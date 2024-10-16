e0888ffa6ea1897d2fca612666b3531212d7a394
Jonas Meisner <meisnerucph@gmail.com>
2024-10-16 11:08:01 +0200

    return of the king

--- setup.py
+++ setup.py
@@ -14,6 +14,29 @@ extensions = [
 ]
 
 setup(
+	name="halkoSVD",
+	version="0.2.3",
+	author="Jonas Meisner",
+	author_email="meisnerucph@gmail.com",
+	description="Fast Python/Cython implementation of the PCAone Halko algorithm",
+	long_description_content_type="text/markdown",
+	long_description=open("README.md").read(),
+	url="https://github.com/Rosemeis/halkoSVD",
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=">=3.7",
+	install_requires=[
+		"cython>3.0.0",
+		"numpy>2.0.0",
+	],
+	packages=["halko"],
+	entry_points={
+		"console_scripts": ["halkoSVD=halko.main:main"]
+	},
     ext_modules=cythonize(extensions),
     include_dirs=[numpy.get_include()]
 )
